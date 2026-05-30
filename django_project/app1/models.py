642e339ace2f47294e285d9163c2761a225166ac
sunilgaru1 <sunilgaru097@gmail.com>
2026-05-30 17:48:42 +0530

    Added First Model

--- django_project/app1/models.py
+++ django_project/app1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
+class appVarity(models.Model):
+
+    APP_TYPE_CHOICE = [
+        ('ML','MOBILE_LEAGE'),
+        ('FF','FREE_FIRE'),
+        ('YT','YOUTUBE'),
+        ('VS','VSCODE'),
+        ('WA','WHATSAPP'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='app1s/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=APP_TYPE_CHOICE)
